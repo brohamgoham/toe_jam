6926117883739e4bd285aea8c65119c4be8ce24f
Mohammed M <mohamgoham@gmail.com>
2023-05-19 06:45:54 -0400

    add mouse wheel events

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use bevy::{
     text, window::WindowMode, a11y::AccessibilityNode,
     a11y::{
         accesskit::{NodeBuilder, Role},
-    },
+    }, input::mouse::{MouseScrollUnit, MouseWheel},
 };
 use bevy_text::{Text, TextSection, TextStyle};
 
@@ -373,4 +373,28 @@ fn title(mut commands: Commands) {
         },
         ..Default::default()
     });
+}
+
+fn mouse_scrolling(
+    mut mouse_wheel_events: EventReader<MouseWheel>,
+    mut query_list: Query<(&mut ScrollingList, &mut Style, &Parent, &Node)>,
+    query_node: Query<&Node>
+) {
+    for mouse_wheel_event in mouse_wheel_events.iter() {
+        for (mut scrolling_list, mut style, parent, list_node) in &mut query_list {
+            let items_height = list_node.size().y;
+            let container_height = query_node.get(parent.get()).unwrap().size().y;
+
+            let max_scroll = (items_height - container_height).max(0.0);
+
+            let dy = match mouse_wheel_event.unit {
+                MouseScrollUnit::Line => mouse_wheel_event.y * 20.0,
+                MouseScrollUnit::Pixel => mouse_wheel_event.y
+            };
+
+            scrolling_list.position += dy;
+            scrolling_list.position = scrolling_list.position.clamp(-max_scroll, 0.0);
+            style.position.top = Val::Px(scrolling_list.position);
+        }
+    }
 }
